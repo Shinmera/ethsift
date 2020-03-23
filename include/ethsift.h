c9c24c7d345db78884e0e0282fb1a012c7b5619c
janleu <janleu@student.ethz.ch>
2020-03-23 21:19:02 +0100

    Edit ethsift.h

--- include/ethsift.h
+++ include/ethsift.h
@@ -47,7 +47,7 @@ extern "C" {
   int ethsift_generate_gaussian_kernel(float *kernel, uint32_t kernel_size, uint32_t kernerl_rad, float sigma);
 
   // Apply the gaussian kernel to the image and write the result to the output.
-  int ethsift_apply_kernel(struct ethsift_image image, float *kernel, uint32_t kernel_size, struct ethsift_image output);
+  int ethsift_apply_kernel(struct ethsift_image image, float *kernel, uint32_t kernel_size, uint32_t kernel_rad, struct ethsift_image output);
 
   // Downscale the image linearly by half and write the result to the output.
   int ethsift_downscale_linear(struct ethsift_image image, struct ethsift_image output);
