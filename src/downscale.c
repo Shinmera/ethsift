663642590545d0e9acfe37d2abbedebd920af703
farkas93 <zsombor.kalotay@gmx.ch>
2020-03-23 23:24:46 +0100

    [NOT TESTED] Downscale implementation

--- src/downscale.c
+++ src/downscale.c
@@ -7,5 +7,18 @@
 /// <param name="output"> OUT: Downscaled image. </param>
 /// <returns> 1 IF generation was successful, ELSE 0. </returns>
 int ethsift_downscale_linear(struct ethsift_image image, struct ethsift_image output){
-  return 0;
+  int srcW = image.width, srcH = image.height;
+  int dstW = srcW >> 1, dstH = srcH >> 1;
+  output.width = srcW;
+  output.height = srcH;
+  //output.pixels = (float*) malloc(secW*srcH*sizeof(float));
+
+  for (int r = 0; r < dstH; r++) {
+    for (int c = 0; c < dstW; c++) {
+      int ori_r = r << 1;
+      int ori_c = c << 1;
+      output.pixels[r * dstW + c] = output.pixels[ori_r * srcW + ori_c];
+    }
+  }
+  return 1;
 }
