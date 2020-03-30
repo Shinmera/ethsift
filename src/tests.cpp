42e6d4b7094349597560972a4644cb902b483e28
Shinmera <shinmera@tymoon.eu>
2020-03-30 17:38:15 +0200

    Pyramid allocation

--- src/tests.cpp
+++ src/tests.cpp
@@ -3,3 +3,25 @@
 define_test(Dummy, {
     return 1;
   })
+
+define_test(SimpleAllocation, {
+    const int pyramid_size = 5;
+    struct ethsift_image pyramid[pyramid_size] = {0};
+    pyramid[0].width = 1024;
+    pyramid[0].height = 512;
+    with_measurement({
+        if(!ethsift_allocate_pyramid(pyramid, pyramid_size))
+          fail("Allocation failed");
+      });
+
+    for(int i=1; i<pyramid_size; ++i){
+      uint32_t width = pyramid[0].width / (2 << (i-1));
+      uint32_t height = pyramid[0].height / (2 << (i-1));
+      if(pyramid[i].pixels == 0)
+        fail("Pixel array unset");
+      if(pyramid[i].width != width)
+        fail("Unexpected width: %i expected %i", pyramid[i].width, width);
+      if(pyramid[i].height != height)
+        fail("Unexpected height: %i expected %i", pyramid[i].height, height);
+    }
+  })
