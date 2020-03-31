1ea19b50224a9117c4c355e86b991c9e380dd72a
janleu <janleu@student.ethz.ch>
2020-03-31 12:27:51 +0200

    Edit refine_local_extrema.c

--- src/refine_local_extrema.c
+++ src/refine_local_extrema.c
@@ -1,11 +1,42 @@
 #include "internal.h"
 
+int mat_dot_vec_3x3(float (*m)[3], float v[], float p[]) {
+  p[0] = m[0][0] * v[0] + m[0][1] * v[1] + m[0][2] * v[2];
+  p[1] = m[1][0] * v[0] + m[1][1] * v[1] + m[1][2] * v[2];
+  p[2] = m[2][0] * v[0] + m[2][1] * v[1] + m[2][2] * v[2]; 
+
+  return 1;
+}
+
+int scale_adjoint_3x3(float (*a)[3], float (*m)[3], float s) {
+  a[0][0] = (s) * (m[1][1] * m[2][2] - m[1][2] * m[2][1]);
+  a[1][0] = (s) * (m[1][2] * m[2][0] - m[1][0] * m[2][2]);
+  a[2][0] = (s) * (m[1][0] * m[2][1] - m[1][1] * m[2][0]);
+                                                                         
+  a[0][1] = (s) * (m[0][2] * m[2][1] - m[0][1] * m[2][2]);
+  a[1][1] = (s) * (m[0][0] * m[2][2] - m[0][2] * m[2][0]);
+  a[2][1] = (s) * (m[0][1] * m[2][0] - m[0][0] * m[2][1]);
+                                                                        
+  a[0][2] = (s) * (m[0][1] * m[1][2] - m[0][2] * m[1][1]);
+  a[1][2] = (s) * (m[0][2] * m[1][0] - m[0][0] * m[1][2]);
+  a[2][2] = (s) * (m[0][0] * m[1][1] - m[0][1] * m[1][0]);
+
+  return 1;
+}
+
+
 // Refine the location of the keypoints to be sub-pixel accurate.
 int ethsift_refine_local_extrema(struct ethsift_image differences[], uint32_t octaves, uint32_t layers, struct ethsift_keypoint *keypoint){
   
   // Settings of EzSift:
+  // TODO Move to settings eventually
+  int SIFT_INTVLS = 3;
   int SIFT_MAX_INTERP_STEPS = 5;
-  int SIFT_KEYPOINT_SUBPiXEL_THR = 0.6f;
+  float SIFT_KEYPOINT_SUBPiXEL_THR = 0.6f;
+  float SIFT_CONTR_THR = 8.0f;
+  float SIFT_CURV_THR = 10.0f;
+  float SIFT_SIGMA = 1.6f;
+  int SIFT_IMG_DBL = 0;
 
   // Fields:
   int w = 0;
@@ -37,7 +68,8 @@ int ethsift_refine_local_extrema(struct ethsift_image differences[], uint32_t oc
 
   // Interpolation (x,y,sigma) 3D space to find sub-pixel accurate
   // location of keypoints.
-  for (int i = 0; i < SIFT_MAX_INTERP_STEPS; ++i) {
+  int i;
+  for (i = 0; i < SIFT_MAX_INTERP_STEPS; ++i) {
     c += xc_i;
     r += xr_i;
 
@@ -83,13 +115,81 @@ int ethsift_refine_local_extrema(struct ethsift_image differences[], uint32_t oc
     det += H[0][2] * (H[1][0] * H[2][1] - H[1][1] * H[2][0]); 
 
     float Hinvert[3][3];
-    // TODO:
-    //    - invert H (Scale adjoint 3x3)
-    //    - Update tmp data for keypoint update
+
+    float s = 1.0f / det;
+    // SCALE_ADJOINT_3X3
+    scale_adjoint_3x3(Hinvert, H, s);
+    // Hinvert[0][0] = (s) * (H[1][1] * H[2][2] - H[1][2] * H[2][1]);          
+    // Hinvert[1][0] = (s) * (H[1][2] * H[2][0] - H[1][0] * H[2][2]);          
+    // Hinvert[2][0] = (s) * (H[1][0] * H[2][1] - H[1][1] * H[2][0]);          
+                                                                            
+    // Hinvert[0][1] = (s) * (H[0][2] * H[2][1] - H[0][1] * H[2][2]);          
+    // Hinvert[1][1] = (s) * (H[0][0] * H[2][2] - H[0][2] * H[2][0]);          
+    // Hinvert[2][1] = (s) * (H[0][1] * H[2][0] - H[0][0] * H[2][1]);          
+                                                                            
+    // Hinvert[0][2] = (s) * (H[0][1] * H[1][2] - H[0][2] * H[1][1]);               
+    // Hinvert[1][2] = (s) * (H[0][2] * H[1][0] - H[0][0] * H[1][2]);               
+    // Hinvert[2][2] = (s) * (H[0][0] * H[1][1] - H[0][1] * H[1][0]); 
+
+    // MAT_DOT_VEC_3X3
+    mat_dot_vec_3x3(Hinvert, x_hat, dD);
+
+    xs = x_hat[2];
+    xr = x_hat[1];
+    xc = x_hat[0];
     
+    // Update tmp data for keypoint update.
+    tmp_r = r + xr;
+    tmp_c = c + xc;
+    tmp_layer = layer + xs;
+
+    // Make sure there is room to move for next iteration.
+    xc_i = ((xc >= SIFT_KEYPOINT_SUBPiXEL_THR && c < w - 2) ? 1 : 0) +
+            ((xc <= -SIFT_KEYPOINT_SUBPiXEL_THR && c > 1) ? -1 : 0);
+
+    xr_i = ((xr >= SIFT_KEYPOINT_SUBPiXEL_THR && r < h - 2) ? 1 : 0) +
+            ((xr <= -SIFT_KEYPOINT_SUBPiXEL_THR && r > 1) ? -1 : 0);
+
+    if (xc_i == 0 && xr_i == 0 && xs_i == 0)
+      break;
   }
 
-  // TODO Update keypoint fields and return if viable keypoint
-  
-  return 0;
+  // We MIGHT be able to remove the following two checking conditions.
+  // Condition 1
+  if (i >= SIFT_MAX_INTERP_STEPS) return 0;
+  // Condition 2.
+  if (fabsf(xc) >= 1.5 || fabsf(xr) >= 1.5 || fabsf(xs) >= 1.5) return 0;
+
+  // If (r, c, layer) is out of range, return false.
+  if (tmp_layer < 0 || tmp_layer > (layers - 1) || tmp_r < 0 ||
+      tmp_r > h - 1 || tmp_c < 0 || tmp_c > w - 1)
+    return 0;
+
+  {
+    float value = differences[curData].pixels[r * w + c] + 0.5f * (dx * xc + dy * xr + ds * xs);
+    if (fabsf(value) < SIFT_CONTR_THR)
+      return 0;
+
+    float trH = dxx + dyy;
+    float detH = dxx * dyy - dxy * dxy;
+    float response = (SIFT_CURV_THR + 1) * (SIFT_CURV_THR + 1) / (SIFT_CURV_THR);
+
+    if (detH <= 0 || (trH * trH / detH) >= response)
+      return 0;
+  }
+
+
+  (*keypoint).layer_pos.x = tmp_c;
+  (*keypoint).layer_pos.y = tmp_r;
+  (*keypoint).layer_pos.scale = SIFT_SIGMA * powf(2.0f, tmp_layer / SIFT_INTVLS);
+
+  int firstOctave = SIFT_IMG_DBL ? -1 : 0;
+  float norm = powf(2.0f, (float)(octave + firstOctave));
+
+  // Coordinates in the normalized format (compared to the original image).
+  (*keypoint).global_pos.x = tmp_c * norm;
+  (*keypoint).global_pos.y = tmp_r * norm;
+  (*keypoint).global_pos.scale = (*keypoint).layer_pos.scale * norm;
+
+  return 1;
 }
