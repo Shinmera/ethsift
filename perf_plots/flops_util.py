43346a5d9ee280bf9ee3939cdbc28c5d53ee3a56
cmimprota <costanza.improta@live.it>
2020-05-02 16:45:58 +0200

    Auto stash before merge of "performance_plots" and "origin/performance_plots"

--- perf_plots/flops_util.py
+++ perf_plots/flops_util.py
@@ -6,6 +6,8 @@ convolution_kernel_size = 9
 calc_layers = 3 # Defualt value set in settings.c
 calc_gaussian_count = calc_layers + 3
 calc_octave_count = lambda w, h: (int) (math.log2(min([w, h])) - 3)
+calc_fast_atan2_f = lambda y: 10 if (y < 0) else 9 
+calc_get_pixel_f = 0
 
 exp_flops = 1
 ceilf_flops = 1
@@ -20,8 +22,8 @@ flops_util['eth']['Convolution'] = lambda w, h: 4 * w * h * convolution_kernel_s
 flops_util['eth']['Octaves'] = lambda w, h: 0 # Conducts only memcpy
 flops_util['eth']['GaussianKernelGeneration'] = lambda w, h: calc_gaussian_count * (powf_flops + sqrt_flops + ceilf_flops + 7 + (get_kernel_sizes(calc_gaussian_count)*(6.0+exp_flops) + calc_gaussian_count)
 flops_util['eth']['GaussianPyramid'] = lambda w, h: flops_util['eth']['GaussianKernelGeneration'](w,h) +  ((calc_gaussian_count-1) * calc_octave_count + 1) * flops_util['eth']['Convolution'](w,h)
-flops_util['eth']['DOGPyramid'] = lambda w, h:     w*h
-flops_util['eth']['GradientAndRotationPyramids'] = lambda w, h:     w*h
+flops_util['eth']['DOGPyramid'] = lambda w, h: calc_octave_count * 5 * w * h
+flops_util['eth']['GradientAndRotationPyramids'] = lambda w, h: calc_octave_count * 3 * w * h * (2 + (4 * calc_get_pixel_f) + sqrt + calc_fast_atan2_f(0))
 flops_util['eth']['Histogram'] = lambda w, h:     w*h
 flops_util['eth']['ExtremaRefinement'] = lambda w, h:     w*h
 flops_util['eth']['KeypointDetection'] = lambda w, h:     w*h
@@ -44,8 +46,8 @@ def get_kernel_sizes(gaussian_count):
     kernel_sizes = np.zeros(gaussian_count)
     k = pow(2.0, 1.0 / calc_layers)
 
-    sigma_pre = 0.5 # Defualt value set in settings.c
-    sigma0 = 1.6 # Defualt value set in settings.c
+    sigma_pre = 0.5 # Default value set in settings.c
+    sigma0 = 1.6 # Default value set in settings.c
 
     sigma_i = np.sqrtf(sigma0 * sigma0 - sigma_pre * sigma_pre)
     curr_rad = sigma_i * 3.0
