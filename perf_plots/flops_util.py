823c398f9b89e4760b56549ef672483485d312fd
Costanza Maria Improta <costanza.improta@live.it>
2020-05-02 15:48:16 +0200

    baseline flop counters

--- perf_plots/flops_util.py
+++ perf_plots/flops_util.py
@@ -1,27 +1,34 @@
+import math
+
 flops_util = dict()
 
 flops_util['eth'] = dict()
-flops_util['eth']['Downscale'] = lambda  w, h:     1 # Conducts only memcpy
-flops_util['eth']['Convolution'] = lambda w, h:     w*h
-flops_util['eth']['Octaves'] = lambda w, h:     w*h
-flops_util['eth']['GaussianKernelGeneration'] = lambda w, h:     w*h
-flops_util['eth']['GaussianPyramid'] = lambda w, h:     w*h
-flops_util['eth']['DOGPyramid'] = lambda w, h:     w*h
-flops_util['eth']['GradientAndRotationPyramids'] = lambda w, h:     w*h
-flops_util['eth']['Histogram'] = lambda w, h:     w*h
-flops_util['eth']['ExtremaRefinement'] = lambda w, h:     w*h
-flops_util['eth']['KeypointDetection'] = lambda w, h:     w*h
-flops_util['eth']['ExtractDescriptor'] = lambda w, h:     w*h
+
+calc_layers = 3
+calc_gaussian_count = calc_layers + 3
+calc_octave_count = lambda w, h: (int) (math.log2(min([w, h])) - 3)
+
+flops_util['eth']['Downscale'] = lambda  w, h:     0 # Conducts only memcpy
+flops_util['eth']['Convolution'] = lambda w, h:     w * h
+flops_util['eth']['Octaves'] = lambda w, h:     w * h
+flops_util['eth']['GaussianKernelGeneration'] = lambda w, h:     w * h
+flops_util['eth']['GaussianPyramid'] = lambda w, h:     w * h
+flops_util['eth']['DOGPyramid'] = lambda w, h:     w * h
+flops_util['eth']['GradientAndRotationPyramids'] = lambda w, h:     w * h
+flops_util['eth']['Histogram'] = lambda w, h:     w * h
+flops_util['eth']['ExtremaRefinement'] = lambda w, h:     w * h
+flops_util['eth']['KeypointDetection'] = lambda w, h:     w * h
+flops_util['eth']['ExtractDescriptor'] = lambda w, h:     w * h
 
 flops_util['ez'] = dict()
 flops_util['ez']['Downscale'] = lambda w, h:      1 
-flops_util['ez']['Convolution'] = lambda w, h:     w*h
-flops_util['ez']['Octaves'] = lambda w, h:     w*h
-flops_util['ez']['GaussianKernelGeneration'] = lambda w, h:     w*h
-flops_util['ez']['GaussianPyramid'] = lambda w, h:     w*h
-flops_util['ez']['DOGPyramid'] = lambda w, h:     w*h
-flops_util['ez']['GradientAndRotationPyramids'] = lambda w, h:     w*h
-flops_util['ez']['Histogram'] = lambda w, h:     w*h
-flops_util['ez']['ExtremaRefinement'] = lambda w, h:     w*h
-flops_util['ez']['KeypointDetection'] = lambda w, h:     w*h
-flops_util['ez']['ExtractDescriptor'] = lambda w, h:     w*h
+flops_util['ez']['Convolution'] = lambda w, h:     w * h
+flops_util['ez']['Octaves'] = lambda w, h:     w * h
+flops_util['ez']['GaussianKernelGeneration'] = lambda w, h:     w * h
+flops_util['ez']['GaussianPyramid'] = lambda w, h:     w * h
+flops_util['ez']['DOGPyramid'] = lambda w, h:     w * h
+flops_util['ez']['GradientAndRotationPyramids'] = lambda w, h:     w * h
+flops_util['ez']['Histogram'] = lambda w, h:     w * h
+flops_util['ez']['ExtremaRefinement'] = lambda w, h:     w * h
+flops_util['ez']['KeypointDetection'] = lambda w, h:     w * h
+flops_util['ez']['ExtractDescriptor'] = lambda w, h:     w * h
