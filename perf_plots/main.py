516c6bb04ce606e8866d8f681585dea6f3d0abbb
Costanza Maria Improta <costanza.improta@live.it>
2020-05-16 12:06:04 +0200

    Zsombors is right, better to have a tab20x for the runtime ones

--- perf_plots/main.py
+++ perf_plots/main.py
@@ -150,7 +150,7 @@ def make_runtime_plot(measurements, cycle_measurement_method, show_plot=True, au
             longest_runtime = max(temp_max, longest_runtime)
 
     p = RuntimePlot(y_max=longest_runtime, meas_method=cycle_measurement_method)
-    col_map =cm.get_cmap('Dark2', nr_lines)
+    col_map =cm.get_cmap('tab20b', nr_lines)
     colors = col_map(np.linspace(0, 1, nr_lines))
     it = 0
 
