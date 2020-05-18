6b58bb1b355ea2092a1e89bb1a0007691e7ab38c
farkas93 <zsombor.kalotay@gmx.ch>
2020-05-18 20:29:03 +0200

    GETTING SEGFAULT: grad_rot_pyramid done with vectorization!

--- src/grad_rot_pyramid.c
+++ src/grad_rot_pyramid.c
@@ -20,6 +20,11 @@ int ethsift_generate_gradient_pyramid(struct ethsift_image gaussians[],
                                       uint32_t octave_count){
     int width, height;
     int idx;
+    
+    float d_row, d_column;
+    float d_row1, d_column1;
+    float d_row2, d_column2;
+
     __m256 d_row_m256, d_column_m256;
     __m256 d_row1_m256, d_column1_m256;
     __m256 d_row2_m256, d_column2_m256;
@@ -58,6 +63,45 @@ int ethsift_generate_gradient_pyramid(struct ethsift_image gaussians[],
 
         inc_mem(9);
 
+        // DO THE THE BORDER OF THE IMAGE AS WE HAVE DONE BEFORE.
+        for(int row = 0; row < height;){
+
+            int row_plus_one = internal_min(internal_max(row + 1, 0), height - 1);
+            int row_minus_one = internal_min(internal_max(row - 1, 0), height - 1);
+            for(int column = 0; column < width;){
+                
+                int col_plus_one = internal_min(internal_max(column + 1, 0), width - 1);
+                int col_minus_one = internal_min(internal_max(column - 1, 0), width - 1);
+
+                d_row = in_gaussian[row_plus_one * width + column] - in_gaussian[row_minus_one * width + column];    
+                d_column = in_gaussian[row * width + col_plus_one] - in_gaussian[row * width + col_minus_one];
+                
+                d_row1 = in_gaussian1[row_plus_one * width + column] - in_gaussian1[row_minus_one * width + column];    
+                d_column1 = in_gaussian1[row * width + col_plus_one] - in_gaussian1[row * width + col_minus_one];
+
+                d_row2 = in_gaussian2[row_plus_one * width + column] - in_gaussian2[row_minus_one * width + column];    
+                d_column2 = in_gaussian2[row * width + col_plus_one] - in_gaussian2[row * width + col_minus_one];
+                    
+                inc_adds(6); // 2 Subtractions
+                inc_mem(12); // Maybe?
+                    
+                out_grads[row * width + column] = sqrtf(d_row * d_row + d_column * d_column);
+                out_rots[row * width + column] = fast_atan2_f(d_row, d_column); 
+                
+                out_grads1[row * width + column] = sqrtf(d_row1 * d_row1 + d_column1 * d_column1);
+                out_rots1[row * width + column] = fast_atan2_f(d_row1, d_column1); 
+                
+                out_grads2[row * width + column] = sqrtf(d_row2 * d_row2 + d_column2 * d_column2);
+                out_rots2[row * width + column] = fast_atan2_f(d_row2, d_column2); 
+                inc_mem(6); // At least two writes
+                
+                ++column;
+                if(column == 4) column = width-4;
+            }
+            
+            ++row;
+            if(row == 4) row = height-4;
+        }     
 
 
 
@@ -117,7 +161,7 @@ int ethsift_generate_gradient_pyramid(struct ethsift_image gaussians[],
                 d_row2_m256 = _mm256_sub_ps(gaussian2_rpo_cols, gaussian2_rmo_cols);
                 d_column2_m256 =  _mm256_sub_ps(gaussian2_cpo_cols, gaussian2_cmo_cols);
                     
-                inc_adds(6); // 2 Subtractions
+                inc_adds(48); // 2 Subtractions
                 inc_mem(12); // Maybe?
                 sqrt_input = _mm256_mul_ps(d_row_m256, d_row_m256);  
                 sqrt_input1 = _mm256_mul_ps(d_row1_m256, d_row1_m256);
@@ -127,12 +171,14 @@ int ethsift_generate_gradient_pyramid(struct ethsift_image gaussians[],
                 sqrt_input1 = _mm256_fmadd_ps(d_column1_m256, d_column1_m256, sqrt_input1);
                 sqrt_input2 = _mm256_fmadd_ps(d_column2_m256, d_column2_m256, sqrt_input2);
 
-                grad = _mm256_sqrt_ps(sqrt_input);
-                rot = _mm256_atan2_ps(d_row_m256, d_column_m256);
+                grad = _mm256_sqrt_ps(sqrt_input);                
+                eth_mm256_atan2_ps(&d_row_m256, &d_column_m256, &rot);
+
                 grad1 = _mm256_sqrt_ps(sqrt_input);
-                rot1 = _mm256_atan2_ps(d_row1_m256, d_column1_m256);
+                eth_mm256_atan2_ps(&d_row1_m256, &d_column1_m256, &rot1);
+
                 grad2 = _mm256_sqrt_ps(sqrt_input);
-                rot2 = _mm256_atan2_ps(d_row2_m256, d_column2_m256);
+                eth_mm256_atan2_ps(&d_row2_m256, &d_column2_m256, &rot2);
 
                 _mm256_store_ps(out_grads + write_index, grad);
                 _mm256_store_ps(out_rots + write_index, rot);
