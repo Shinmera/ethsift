ba4e8b1870896bfddaad1bb9882fba5ecfdbc7ac
farkas93 <zsombor.kalotay@gmx.ch>
2020-05-21 17:21:02 +0200

    include ICC atan2 implementation

--- src/grad_rot_pyramid.c
+++ src/grad_rot_pyramid.c
@@ -132,14 +132,19 @@ int ethsift_generate_gradient_pyramid(struct ethsift_image gaussians[],
                 sqrt_input1 = _mm256_fmadd_ps(d_column1_m256, d_column1_m256, sqrt_input1);
                 sqrt_input2 = _mm256_fmadd_ps(d_column2_m256, d_column2_m256, sqrt_input2);
 
-                grad = _mm256_sqrt_ps(sqrt_input);                
-                eth_mm256_atan2_ps(&d_row_m256, &d_column_m256, &rot);
-
-                grad1 = _mm256_sqrt_ps(sqrt_input1);
-                eth_mm256_atan2_ps(&d_row1_m256, &d_column1_m256, &rot1);
-
+                grad = _mm256_sqrt_ps(sqrt_input);
+                grad1 = _mm256_sqrt_ps(sqrt_input1);                
                 grad2 = _mm256_sqrt_ps(sqrt_input2);
+                
+#ifdef __INTEL_COMPILER
+                rot = _mm256_atan2_ps(d_row_m256, d_column_m256);
+                rot1 = _mm256_atan2_ps(d_row1_m256, d_column1_m256);
+                rot2 = _mm256_atan2_ps(d_row2_m256, d_column2_m256);
+#else                
+                eth_mm256_atan2_ps(&d_row_m256, &d_column_m256, &rot);
+                eth_mm256_atan2_ps(&d_row1_m256, &d_column1_m256, &rot1);
                 eth_mm256_atan2_ps(&d_row2_m256, &d_column2_m256, &rot2);
+#endif
 
                 _mm256_storeu_ps(out_grads + write_index, grad);
                 _mm256_storeu_ps(out_rots + write_index, rot);
